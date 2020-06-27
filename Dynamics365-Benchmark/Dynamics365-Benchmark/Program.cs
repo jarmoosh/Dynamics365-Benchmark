9b30c8ba309827b66684cdd285dc37200aa265f2
Jacek <Jacek@DESKTOP-N048N7C>
2020-06-27 17:53:54 +0200

    add sln and program cs

---
+++ Dynamics365-Benchmark/Dynamics365-Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dynamics365_Benchmark
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+}
